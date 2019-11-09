2cb38fc487287d6d6b95a57a80c854a5602ca4db
Kostas Bastas <costasbastas@gmail.com>
2019-11-09 08:19:24 +0200

    interface creation

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-@Injectable({ providedIn: 'root' })
+@Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  constructor(private _http: Http) {}
+  constructor(private _http: Http) { }
   getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(response => response.json());
   }
