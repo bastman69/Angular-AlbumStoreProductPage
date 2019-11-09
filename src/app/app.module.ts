a0c539e3f9ec37df11d721aeb70609d854e059dd
Kostas Bastas <costasbastas@gmail.com>
2019-11-09 09:39:44 +0200

    part7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,16 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'products' }
+];
 @NgModule({
   declarations: [AppComponent, ProductPageComponent, ProductDescriptionComponent, ProductTracklistingComponent, ProductListComponent],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [BrowserModule, FormsModule, HttpModule, RouterModule, RouterModule.forRoot(appRoutes)],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
